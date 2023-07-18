c7d79c32bc9a3be01e97339ea502862cf8b86a3d
aman-agrawal <9412470@gmail,com>
2023-07-18 22:15:30 +0530

    Junit4 to Junit5 Migration

--- igor-web/src/test/groovy/com/netflix/spinnaker/igor/gcb/GoogleCloudBuildExecutorTest.java
+++ igor-web/src/test/groovy/com/netflix/spinnaker/igor/gcb/GoogleCloudBuildExecutorTest.java
@@ -22,11 +22,8 @@ import static org.mockito.Mockito.when;
 
 import com.google.api.client.json.GenericJson;
 import com.google.api.services.cloudbuild.v1.CloudBuildRequest;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.jupiter.api.Test;
 
-@RunWith(MockitoJUnitRunner.class)
 public class GoogleCloudBuildExecutorTest {
   private GoogleCloudBuildExecutor executor = new GoogleCloudBuildExecutor();
 
